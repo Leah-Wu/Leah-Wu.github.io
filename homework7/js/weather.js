5b19327b9ad64f42a96f103f2c9da32e18dda645
Leah-Wu <54871865+Leah-Wu@users.noreply.github.com>
2020-11-30 19:29:12 -0500

    final

--- homework7/js/weather.js
+++ homework7/js/weather.js
@@ -12,17 +12,26 @@ function gettingJSON(){
     console.log("Location is : " + location);
 
     //set default temperature format if one isn't provided
-    // let format;
-    // format = document.querySelectorAll("input[name=temp]:checked")[0].value;
-    // // Your code here.
-    // console.log("Format is " + format);
+    let format = "imperial";
+    if(document.querySelectorAll("input[name=temp]:checked")[0]){
+        format = document.querySelectorAll("input[name=temp]:checked")[0].value;
+    }
+
+    // Your code here.
+    console.log("Format is " + format);
 
     //set the query  
     let query;
     // Your code here.  
-    query = "api.openweathermap.org/data/2.5/weather?q=" +location+"&appid=37bdd7a970968c72dbf93b42ed08568e";
+    var loc_list = location.split(" ");
+    if(!isNaN(loc_list[0])){
+        query = "https://api.openweathermap.org/data/2.5/weather?zip="+loc_list[0]+"&units="+format+"&appid=37bdd7a970968c72dbf93b42ed08568e";
+    }
+    else {
+        query = "https://api.openweathermap.org/data/2.5/weather?q=" +location+"&units="+format+"&appid=37bdd7a970968c72dbf93b42ed08568e";
+    }
     console.log("Query is :" + query);
-    //"api.openweathermap.org/data/2.5/weather?zip={zip code},{country code}&appid={API key}"
+    //"api.openweathermap.org/data/2.5/weather?zip={zip code},{country code}&appid=37bdd7a970968c72dbf93b42ed08568e"
     
     //Create and set variables for each of the elements you
     //need to update, location, temp, the image, etc.
@@ -39,5 +48,22 @@ function gettingJSON(){
         // Your code here.
         console.log(JSON.stringify(json));
 
+        weather = json["weather"][0]["description"];
+
+        temp = json["main"]["temp"];
+        loc = json['name'];
+
+        document.querySelector("#loc").innerHTML=loc;
+        document.querySelector("#temp").innerHTML=temp + " with " + weather;
+        
+        tempImg=document.querySelector("#tempImg");
+        console.log(json["weather"][0]["icon"]);
+        var source = 'http://openweathermap.org/img/wn/'+ json["weather"][0]["icon"]+'.png';
+        console.log(source);
+        tempImg.src=source;
+        tempImg.alt=weather;
+        tempImg.title="Weather in "+loc;
+        console.log(tempImg.title);
+        document.querySelector("#forecast").style.display="block";
     });
 }
